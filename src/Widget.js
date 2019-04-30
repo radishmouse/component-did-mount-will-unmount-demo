dbfa14913e01f360f79708dbfc6e09b1e16d97f3
chris aquino <chris@radishmouse.com>
2019-04-30 10:01:15 -0400

    adds state and interval to widget

--- src/Widget.js
+++ src/Widget.js
@@ -3,19 +3,33 @@ import React from 'react';
 
 class Widget extends React.Component {
 
+    constructor(props) {
+        super(props);
+        console.log('Widget: constructor');
+        this.state = {
+            aNumber: 0
+        }
+    }
+
     componentDidMount() {
         console.log('Widget: componentDidMount');
+        this.interval = setInterval(() => {
+            this.setState({
+                aNumber: this.state.aNumber + 1
+            });
+        }, 1000);
     }
     
     componentWillUnmount() {        
         console.log('Widget: componentWillUnmount');
+        clearInterval(this.interval);
     }
         
     render() {
         console.log('Widget: render');
         return (
             <div>
-                Hello!
+                Hello! {this.state.aNumber}
             </div>
         );
     }
